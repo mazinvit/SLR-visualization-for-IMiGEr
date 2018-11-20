526d7db138d063b9acf7a6dd9941696fbab4240f
Martin Matas <nouzovekonto@gmail.com>
2018-11-20 14:28:07 +0100

    Added javadoc comments

--- src/main/java/cz/zcu/kiv/fjp/core/parser/DotParser.java
+++ src/main/java/cz/zcu/kiv/fjp/core/parser/DotParser.java
@@ -10,9 +10,14 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Abstract class for parser that extracts vertices and edges from .dot file
+ * and provides methods to converts extracted objects into custom entities and methods
+ * for defining their archetypes.
+ */
 public abstract class DotParser {
 
-    protected File inputFile, outputFile;
+
     protected GraphParser parser;
     protected List<EdgeArchetype> edgeArchetypes;
     protected List<VertexArchetype> vertexArchetypes;
@@ -20,6 +25,11 @@ public abstract class DotParser {
 
     int edgeId;
 
+    /**
+     * Loads graph from .dot file into the structure and defines archetypes for vertices and edges.
+     *
+     * @param inputFile - input file containing graph in DOT form
+     */
     public DotParser(String inputFile) {
         try {
             System.out.println("Parser Initialization.");
@@ -35,33 +45,75 @@ public abstract class DotParser {
         }
     }
 
+    /**
+     * Returns node ID as a hashcode.     *
+     * @param node - parsed node from graph
+     *
+     * @return - hashcode of node ID
+     */
     protected final int getNodeID(GraphNode node) {
         return node.getId().hashCode();
     }
 
+    /**
+     * Defines list of attributes for edges.
+     */
     protected void defineAttributeTypes() {
         attributeTypes = new ArrayList<AttributeType>();
         attributeTypes.add(new AttributeType("string", "label", ""));
     }
 
+    /**
+     * Returns list of archetypes for edges.
+     *
+     * @return list of archetypes for edges
+     */
     public List<EdgeArchetype> getEdgeArchetypes() {
         return edgeArchetypes;
     }
 
+    /**
+     * Returns list of archetypes for vertices.
+     *
+     * @return list of archetypes for vertices
+     */
     public List<VertexArchetype> getVertexArchetypes() {
         return vertexArchetypes;
     }
 
+    /**
+     * Returns list of attribute types for edges.
+     *
+     * @return list of attribute types for edges
+     */
     public List<AttributeType> getAttributeTypes() {
         return attributeTypes;
     }
 
+    /**
+     * Method takes map of vertices from parser and stores them using object
+     * {@link Vertex} into the list of vertices.
+     *
+     * @return list of vertices
+     */
     public abstract List<Vertex> parseNodes();
 
+    /**
+     * Method takes map of edges from parser and stores them using objects
+     * {@link EdgeAttribute} and {@link Edge} into the list of edges.
+     *
+     * @return list of edges
+     */
     public abstract List<Edge> parseEdges();
 
+    /**
+     * Method defines list of archetypes for edges.
+     */
     protected abstract void defineEdgeArchetypes();
 
+    /**
+     * Method defines list of archetypes for vertices.
+     */
     protected abstract void defineVertexArchetypes();
 
 }
